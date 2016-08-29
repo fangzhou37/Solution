b9ebf2fd356c788a88804a348ecf4dd87eeb51b1
fangzhou37 <fangzhou.ark37@gmail.com>
2016-08-28 21:27:10 -0700

    .

---
+++ src/_105ConstructBinaryTreeFromPreorderAndInorderTraversal/Solution.java
@@ -0,0 +1,32 @@
+package _105ConstructBinaryTreeFromPreorderAndInorderTraversal;
+
+public class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return buildTreeHelper(preorder, inorder, 0, preorder.length-1, 0);
+    }
+
+    private TreeNode buildTreeHelper(int[] preorder, int[] inorder, int preStart, int preEnd, int inStart) {
+        if (preStart > preEnd) {
+            return null;
+        }
+        TreeNode cur = new TreeNode(preorder[preStart]);
+        int curIndexInInorder = -1;
+        for (int i = 0; i < inorder.length; i++) {
+            if (preorder[preStart] == inorder[i]) {
+                curIndexInInorder = i;
+                break;
+            }
+        }
+        int leftSubTreeSize = curIndexInInorder - inStart;
+        cur.left = buildTreeHelper(preorder, inorder, preStart + 1, preStart + leftSubTreeSize, inStart);
+        cur.right = buildTreeHelper(preorder, inorder, preStart + leftSubTreeSize + 1, preEnd, curIndexInInorder+1);
+        return cur;
+    }
+
+    public static void main(String[] args) {
+        int[] preorder = new int[] {1,2,4,5,3,6,7};
+        int[] inorder = new int[] {4,2,5,1,6,3,7};
+        TreeNode n = new Solution().buildTree(preorder, inorder);
+        System.out.println();
+    }
+}
---
+++ src/_105ConstructBinaryTreeFromPreorderAndInorderTraversal/TreeNode.java
@@ -0,0 +1,11 @@
+package _105ConstructBinaryTreeFromPreorderAndInorderTraversal;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
