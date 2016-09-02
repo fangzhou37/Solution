be18f40ebe5a74582bfd5c21f4ac9cec60bc1669
fangzhou37 <fangzhou.ark37@gmail.com>
2016-09-01 20:01:05 -0700

    .

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,7 @@
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
 public class Main {
-
-    public ListNode mergeKLists(ListNode[] lists) {
-        ListNode holder = new ListNode(-1), cur = holder;
-        PriorityQueue<ListNode> queue= new PriorityQueue<>(lists.length, new Comparator<ListNode>(){
-            @Override
-            public int compare(ListNode o1, ListNode o2){
-                return o1.val - o2.val;
-            }
-        });
-        for (ListNode n : lists) {
-            if (n != null) {
-                queue.add(n);
-            }
-        }
-
-        while (!queue.isEmpty()) {
-            ListNode n = queue.poll();
-            ListNode next = n.next;
-            cur.next = n;
-            n.next = null;
-            cur = cur.next;
-            if (next != null) {
-                queue.add(next);
-            }
-        }
-
-        return holder.next;
-    }
 
     public static void main(String[] args) {
         System.out.println();
+
     }
 }
