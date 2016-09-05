6eb5c1d3fc6ea0848300216b035e89308bb15655
fangzhou37 <fangzhou.ark37@gmail.com>
2016-09-04 18:05:07 -0700

    .

---
+++ src/_9PalindromeNumber/Solution.java
@@ -0,0 +1,32 @@
+package _9PalindromeNumber;
+
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int pivot = 1, cur = x;
+        while (cur > 9) {
+            pivot *= 10;
+            cur /= 10;
+        }
+        cur = x;
+        // 23432
+        while (cur > 0) {
+            int high = cur/pivot;   // high 2
+            int low = cur%10;   // low 2
+            if (high != low) {
+                return false;
+            }
+            cur = cur % pivot;  // 3432
+            pivot /= 100;    // 1000
+            cur /= 10;  // 343
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().isPalindrome(100021));
+    }
+
+}
