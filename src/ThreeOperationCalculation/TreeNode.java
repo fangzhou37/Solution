7f616eb3acaa92e6eb821d3b06175fff5208d494
fangzhou37 <fangzhou.ark37@gmail.com>
2016-09-07 21:30:18 -0700

    TODO

---
+++ src/ThreeOperationCalculation/TreeNode.java
@@ -0,0 +1,21 @@
+package ThreeOperationCalculation;
+
+public class TreeNode {
+    Boolean val;
+    TreeNode judge;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(Boolean x) {
+        val = x;
+    }
+
+    TreeNode() {}
+
+    public Boolean calculate() {
+        if (judge != null) {
+            val = judge.calculate() ? left.calculate() : right.calculate();
+        }
+        return val;
+    }
+}
