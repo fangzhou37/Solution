55e56a7d6dfaebebcd35efdc74f7edde690b6aaa
fangzhou37 <fangzhou.ark37@gmail.com>
2016-09-10 15:00:26 -0700

    .

---
+++ src/_171ExcelSheetColumnNumber/Solution.java
@@ -0,0 +1,11 @@
+package _171ExcelSheetColumnNumber;
+
+public class Solution {
+    public int titleToNumber(String s) {
+        int res = 0;
+        for (char c : s.toCharArray()) {
+            res = res * 26 + (c-'A'+1);
+        }
+        return res;
+    }
+}
