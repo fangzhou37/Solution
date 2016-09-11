6b76be2565f83942a2dd5af46209399b47940aff
fangzhou37 <fangzhou.ark37@gmail.com>
2016-09-11 11:12:40 -0700

    .

---
+++ src/_263UglyNumber/Solution.java
@@ -0,0 +1,22 @@
+package _263UglyNumber;
+
+public class Solution {
+    public boolean isUgly(int num) {
+        if (num == 1) {
+            return true;
+        }
+        if (num == 0) {
+            return false;
+        }
+        while (num % 2 == 0) {
+            num /= 2;
+        }
+        while (num % 3 == 0) {
+            num /= 3;
+        }
+        while (num % 5 == 0) {
+            num /= 5;
+        }
+        return num == 1;
+    }
+}
