1aef4332fc9fe79845d2a88ba73dc66d57c698a6
fangzhou37 <fangzhou.ark37@gmail.com>
2016-09-12 23:08:16 -0700

    .

--- src/_376WiggleSubsequence/Solution.java
+++ src/_376WiggleSubsequence/Solution.java
@@ -17,13 +17,15 @@ public class Solution {
                 res++;
                 neverFindPprev = false;
             }
-            pprev = prev;
+            pprev = prev;   // 注意这里也要更新，即使当前非拐点
             prev = i;
         }
         return res;
     }
 
     public static void main(String[] args) {
+        // 64，50，41，45， 21，97
+        // 如果到41的时候，不更新pprev和prev，那么就会得到64，50，97，而错失41，45， 21，97
         System.out.println(new Solution().wiggleMaxLength(new int[] {64,50,41,45,21,97,35,47}));
     }
 }
