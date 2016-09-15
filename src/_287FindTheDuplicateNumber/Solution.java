22a78cd996b1a477a0a98b0e6c9cf97d9e90d237
fangzhou37 <fangzhou.ark37@gmail.com>
2016-09-14 20:52:39 -0700

    .

---
+++ src/_287FindTheDuplicateNumber/Solution.java
@@ -0,0 +1,24 @@
+package _287FindTheDuplicateNumber;
+
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        if (nums.length > 1) {
+            int slow = nums[0], fast = nums[nums[0]];
+            while (slow != fast) {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            }
+            fast = 0;
+            while (slow != fast) {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            }
+            return slow;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().findDuplicate(new int[]{3,2,4,1,5,4}));
+    }
+}
