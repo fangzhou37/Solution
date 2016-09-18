75965c6b6ef4e0810ca66ec8e40e878727d2e3af
fangzhou37 <fangzhou.ark37@gmail.com>
2016-09-18 14:20:30 -0700

    .

---
+++ src/_401BinaryWatch/Solution.java
@@ -0,0 +1,18 @@
+package _401BinaryWatch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<String> readBinaryWatch(int num) {
+        List<String> res = new ArrayList<>();
+        for (int h = 0; h < 12; h++) {
+            for (int m = 0; m < 60; m++) {
+                if (Integer.bitCount(h) + Integer.bitCount(m) == num) {
+                    res.add(String.format("%d:%02d", h, m));
+                }
+            }
+        }
+        return res;
+    }
+}
