1e131df0474cfca7b2d4ccd1362941ce18f728eb
fangzhou37 <fangzhou.ark37@gmail.com>
2016-09-17 23:06:23 -0700

    .

--- src/_337HouseRobberIII/Solution.java
+++ src/_337HouseRobberIII/Solution.java
@@ -1,41 +1,19 @@
 package _337HouseRobberIII;
 
-import java.util.*;
-
 public class Solution {
     public int rob(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        List<TreeNode> cur = new LinkedList<>();
-        cur.add(root);
-        List<Integer> levelSum = new ArrayList<>();
-        while (!cur.isEmpty()) {
-            List<TreeNode> next = new LinkedList<>();
-            int currentLevelSum = 0;
-            for (TreeNode c : cur) {
-                currentLevelSum += c.val;
-                if (c.left != null) {
-                    next.add(c.left);
-                }
-                if (c.right != null) {
-                    next.add(c.right);
-                }
-            }
-            levelSum.add(currentLevelSum);
-            cur = next;
+        int max = 0;
+        if (root.left != null) {
+            max += rob(root.left.left) + rob(root.left.right);
         }
-        for (int i = 2; i < levelSum.size(); i++) {
-            int largestSumThatEndsInThisLevel = levelSum.get(i);
-            if (i == 2) {
-                largestSumThatEndsInThisLevel += levelSum.get(i-2);
-            }
-            if (i > 2) {
-                largestSumThatEndsInThisLevel += Math.max(levelSum.get(i-2), levelSum.get(i-3));
-            }
-            levelSum.set(i, largestSumThatEndsInThisLevel);
+        if (root.right != null) {
+            max += rob(root.right.left) + rob(root.right.right);
         }
-        return levelSum.size() > 1 ? Math.max(levelSum.get(levelSum.size()-1), levelSum.get(levelSum.size()-2)) : levelSum.get(0);
+        max += root.val;
+        return Math.max(max, rob(root.left) + rob(root.right));
     }
 
     public static void main(String[] args) {
@@ -43,6 +21,6 @@ public class Solution {
         head.left = new TreeNode(1);
         head.left.right = new TreeNode(4);
         head.right = new TreeNode(3);
-        new Solution().rob(head);
+        System.out.println(new Solution().rob(head));
     }
 }
