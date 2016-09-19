4fc7a2ced0267b1bc8df7f14798a75d85e065ecb
fangzhou37 <fangzhou.ark37@gmail.com>
2016-09-19 00:24:13 -0700

    .

--- src/_290WordPattern/Solution.java
+++ src/_290WordPattern/Solution.java
@@ -9,7 +9,7 @@ public class Solution {
         if (words.length != pattern.length()) {
             return false;
         }
-        Map<Object, Object> m = new HashMap();
+        Map<Object, Object> m = new HashMap();  // 不指明type有时候也很有用，当作一个布袋
         // 如果用int i = 0, 会出现意想不到的情况
         for (Integer i = 0; i < words.length; i++) {
             // hashmap put() would return the previous value associated with key, or null if there was no mapping for key.
