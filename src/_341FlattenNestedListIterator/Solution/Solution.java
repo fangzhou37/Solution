4451a1974a1fc37df0c2be4e1a7142f7e59bb7cf
fangzhou37 <fangzhou.ark37@gmail.com>
2016-09-19 22:46:44 -0700

    .

---
+++ src/_341FlattenNestedListIterator/Solution/Solution.java
@@ -0,0 +1,60 @@
+package _341FlattenNestedListIterator.Solution;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+interface NestedInteger {
+
+    // @return true if this NestedInteger holds a single integer, rather than a nested list.
+    public boolean isInteger();
+
+    // @return the single integer that this NestedInteger holds, if it holds a single integer
+    // Return null if this NestedInteger holds a nested list
+    public Integer getInteger();
+
+    // @return the nested list that this NestedInteger holds, if it holds a nested list
+    // Return null if this NestedInteger holds a single integer
+    public List<NestedInteger> getList();
+}
+
+class NestedIterator implements Iterator<Integer> {
+    Stack<NestedInteger> stack;
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        stack = new Stack<>();
+        for(int i = nestedList.size() - 1; i >= 0; i--) {
+            stack.push(nestedList.get(i));
+        }
+    }
+
+    @Override
+    public Integer next() {
+        return stack.pop().getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (!stack.isEmpty()) {
+            NestedInteger cur = stack.peek();
+            if (cur.isInteger()) {
+                return true;
+            }
+            stack.pop();
+            List<NestedInteger> curList = cur.getList();
+            for (int i = curList.size()-1; i >= 0; i--) {
+                stack.push(curList.get(i));
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void remove() {
+
+    }
+}
+
+public class Solution {
+
+}
