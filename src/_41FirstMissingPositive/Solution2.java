752659c286239b533eedef613b96f25df5327756
fangzhou37 <fangzhou.ark37@gmail.com>
2016-09-30 21:42:16 -0700

    .

---
+++ src/_41FirstMissingPositive/Solution2.java
@@ -0,0 +1,26 @@
+package _41FirstMissingPositive;
+
+public class Solution2 {
+    public int firstMissingPositive(int[] nums) {
+        if (nums.length == 0) {
+            return 1;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] - 1 >= 0 && nums[i] - 1 < nums.length && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i+1) {
+                return i+1;
+            }
+        }
+        return nums.length + 1;
+    }
+
+    private static void swap(int[] nums, int a, int b) {
+        int t = nums[a];
+        nums[a] = nums[b];
+        nums[b] = t;
+    }
+}
