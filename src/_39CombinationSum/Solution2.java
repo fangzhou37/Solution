de51754a33a81420426033dbe3b234b437e6c258
fangzhou37 <fangzhou.ark37@gmail.com>
2016-09-30 21:24:57 -0700

    .

---
+++ src/_39CombinationSum/Solution2.java
@@ -0,0 +1,36 @@
+package _39CombinationSum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution2 {
+    // Cannot use two pointers
+    // DP turns to be hard since this requires to return path
+    // DFS
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new LinkedList<>();
+        Arrays.sort(candidates);
+        LinkedList<Integer> buffer = new LinkedList<>();
+        dfs(candidates, 0, buffer, target, res);
+        return res;
+    }
+
+    private void dfs(int[] candidates, int i, LinkedList<Integer> buffer, int target, List<List<Integer>> res) {
+        if (i >= candidates.length) {
+            return;
+        }
+        if (target == 0) {
+            res.add(new LinkedList<>(buffer));
+            return;
+        }
+        if (candidates[i] > target) {
+            return;
+        }
+        dfs(candidates, i+1, buffer, target, res);
+        buffer.addLast(candidates[i]);
+        dfs(candidates, i, buffer, target - candidates[i], res);
+        buffer.removeLast();
+    }
+}
