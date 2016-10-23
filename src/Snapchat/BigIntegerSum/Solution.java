2c1d89323681867eb6713e1f8a85a3c8f6a5ec11
fangzhou37 <fangzhou.ark37@gmail.com>
2016-10-22 18:53:28 -0700

    .

---
+++ src/Snapchat/BigIntegerSum/Solution.java
@@ -0,0 +1,33 @@
+package Snapchat.BigIntegerSum;
+
+public class Solution {
+    public String add(String s1, String s2){
+        StringBuffer sb = new StringBuffer();
+        int carry = 0;
+        s1 = new StringBuffer(s1).reverse().toString();
+        s2 = new StringBuffer(s2).reverse().toString();
+        int i = 0;
+        for (; i < s1.length() || i < s2.length(); i++) {
+            if (i < s1.length()) {
+                carry += s1.charAt(i) - '0';    // 易错
+            }
+            if (i < s2.length()) {
+                carry += s2.charAt(i) - '0';
+            }
+            sb.append(carry % 10);
+            carry /= 10;
+        }
+        if (carry != 0) {
+            sb.append(carry);
+        }
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s1 = "1231231";
+        String s2 = "234234";
+        System.out.println(solution.add(s1,s2));
+        System.out.println(Integer.valueOf(s1) + Integer.valueOf(s2));
+    }
+}
