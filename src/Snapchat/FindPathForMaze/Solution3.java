a43936e5379d1c4ba6c2c17540a31cb27d779d21
fangzhou37 <fangzhou.ark37@gmail.com>
2016-11-04 23:54:57 -0700

    .

--- src/Snapchat/FindPathForMaze/Solution3.java
+++ src/Snapchat/FindPathForMaze/Solution3.java
@@ -5,7 +5,8 @@ import java.util.Queue;
 
 public class Solution3 {
     /*
-    "1"代表起点，位于左上角；"9"代表重点，位于右下角；"0"代表通路，"5"代表墙。. 矩阵长得是这样
+    从左上角走到右下角
+    "0"代表通路，"5"代表墙。. 矩阵长得是这样
     [
         [1, 5, 5, 5, 5, 0],
         [0, 5, 0, 5, 5, 0],
@@ -22,18 +23,8 @@ public class Solution3 {
         if (matrix.length == 0 || matrix[0].length == 0) {
             return false;
         }
-        int[] start = new int[2];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] == 1) {
-                    start[0] = i;
-                    start[1] = j;
-                    break;
-                }
-            }
-        }
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {start[0], start[1]});
+        queue.offer(new int[] {0, 0});
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
             if (matrix[cur[0]][cur[1]] == -1) {  // has been visited
@@ -46,7 +37,7 @@ public class Solution3 {
                 if (x < 0 || y < 0 || x >= matrix.length || y >= matrix[0].length) {
                     continue;
                 }
-                if (matrix[x][y] == 9) {
+                if (x == matrix.length - 1 && y == matrix[0].length - 1) {
                     return true;
                 }
                 if (matrix[x][y] == 0) {
@@ -59,11 +50,11 @@ public class Solution3 {
 
     public static void main(String[] args) {
         int[][] matrix = new int[][]{
-                {1, 5, 5, 5, 5, 0},
+                {0, 5, 5, 5, 5, 0},
                 {0, 5, 0, 5, 5, 0},
                 {0, 5, 0, 0, 0, 0},
                 {0, 5, 0, 0, 5, 0},
-                {0, 0, 0, 5, 0, 9},
+                {0, 0, 0, 5, 0, 0},
         };
         System.out.println("----------------------------");
         System.out.println(new Solution3().havePath(matrix));
